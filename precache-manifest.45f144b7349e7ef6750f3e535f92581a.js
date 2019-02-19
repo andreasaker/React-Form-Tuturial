249b34a49ee5180cc2dbfee96b960cf9010cec88
Andreas <andreas.aker91@gmail.com>
2019-02-19 21:22:27 +0100

    Updates

---
+++ precache-manifest.45f144b7349e7ef6750f3e535f92581a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "e56b422e87bab4f9f79c",
+    "url": "/react-tutorial/static/js/main.e56b422e.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "e56b422e87bab4f9f79c",
+    "url": "/react-tutorial/static/css/main.8d01aa32.chunk.css"
+  },
+  {
+    "revision": "a15999902592133d9d1001f5a6fc7e79",
+    "url": "/react-tutorial/index.html"
+  }
+];
